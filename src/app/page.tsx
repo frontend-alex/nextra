0b61629ea5d3b46adc149b8e831c59eb8f02fa9f
frontend-alex <alexmmech@gmail.com>
2024-08-17 17:40:06 +0300

    circle component integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@components/ui/button";
+import Circle from "@components/ui/circle";
 import { ContainerScroll } from "@components/ui/container-scroll-animation";
 import { Spotlight } from "@components/ui/sportlight";
 
@@ -19,7 +20,8 @@ const Page = () => {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-        <div className="max-w-wrapper flex-col-3 items-center text-center">
+        <div className="relative max-w-wrapper flex-col-3 items-center text-center">
+          <Circle show={true} className="h-[100px] w-[100px] left-32 top-10 z-[-1]"/>
           <h1 className="text-3xl lg:text-6xl font-bold">
             {t("headerHeading")}
           </h1>
@@ -29,7 +31,8 @@ const Page = () => {
             <Button variant="outline">{t('learnMore')}</Button>
           </div>
         </div>
-        <div className="flex flex-col overflow-hidden">
+        <div className="relative flex flex-col overflow-hidden bg">
+          <Circle show={false} className="h-[100px] w-[100px] right-32 top-10"/>
           <ContainerScroll titleComponent={""}>
             <Image
               src={`https://colorlib.com/wp/wp-content/uploads/sites/2/free-dashboard-templates-1.jpg`}
